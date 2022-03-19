828309e23b7bc3039b52b193e87c570c64da00df
lucasaraujo6 <lucas_araujo_11@hotmail.com>
2022-03-19 19:59:27 -0300

    Versão simplificada da listagem funcionando. Reordenamento também.

--- app/src/main/java/com/jogos/listadecompras/ItemRecViewAdapter.java
+++ app/src/main/java/com/jogos/listadecompras/ItemRecViewAdapter.java
@@ -1,11 +1,9 @@
 package com.jogos.listadecompras;
 
-import static android.content.ContentValues.TAG;
+import static android.view.DragEvent.ACTION_DROP;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.util.Log;
-import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,20 +12,36 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
 
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Locale;
 
 public class ItemRecViewAdapter extends RecyclerView.Adapter<ItemRecViewAdapter.ViewHolder>
+
 {
 
+    public Context getContext;
     private ArrayList<Item> items = new ArrayList<>();
     private Context mContext;
+    private int fromPosition;
+    private int toPosition;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//    private LayoutParams layoutParams;
+//    private static final String RECYCLERVIEW_TAG = "icon bitmap";
+    private ArrayList<Integer> positions;
+    private boolean theresChange;
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
     public ItemRecViewAdapter(Context mContext) {
@@ -46,19 +60,26 @@ public class ItemRecViewAdapter extends RecyclerView.Adapter<ItemRecViewAdapter.
         holder.txtName.setText(items.get(position).getName());
         holder.txtQntd.setText(items.get(position).getAmount());
         holder.checkBox.setChecked(items.get(position).isChecked());
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//        holder.parent.setTag(RECYCLERVIEW_TAG);
+
+
         holder.checkBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 items.get(position).setChecked(items.get(position).isChecked());
                 holder.txtName.setText(""+items.get(position).isChecked()+items.get(position).getName());
-        //        notifyDataSetChanged();
+        //        notifyDataSetCha
+                //        ged();
 
             }
         });
-        Glide.with(mContext)
-                .asBitmap()
-                .load(items.get(position).getImageUrl())
-                .into(holder.imageItem);
+//        Glide.with(mContext)
+//                .asBitmap()
+//                .load(items.get(position).getImageUrl())
+//                .into(holder.imageItem);
 
         holder.parent.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +88,77 @@ public class ItemRecViewAdapter extends RecyclerView.Adapter<ItemRecViewAdapter.
             }
         });
 
+//        holder.parent.setOnLongClickListener(new View.OnLongClickListener() {
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//            @Override
+//            public boolean onLongClick(View v) {
+//                ClipData.Item item = new ClipData.Item((CharSequence)v.getTag());
+//                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+//
+//                ClipData dragData = new ClipData(v.getTag().toString(),mimeTypes, item);
+//
+//                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(holder.parent);
+//
+//
+//                v.startDrag(dragData,myShadow,null,0);
+//
+//                return true;
+//            }
+//        });
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//        holder.parent.setOnDragListener(new View.OnDragListener() {
+//            @Override
+//            public boolean onDrag(View v, DragEvent event) {
+//                switch(event.getAction()) {
+//                    case DragEvent.ACTION_DRAG_STARTED:
+////                        Toast.makeText(mContext, "Action is DragEvent.ACTION_DRAG_STARTED", Toast.LENGTH_SHORT).show();
+//
+//                        break;
+//
+//                    case DragEvent.ACTION_DRAG_ENTERED:
+//                        Toast.makeText(mContext, "Action is DragEvent.ACTION_DRAG_ENTERED", Toast.LENGTH_SHORT).show();
+//
+//
+//                        break;
+//
+//                    case DragEvent.ACTION_DRAG_EXITED :
+//                        Toast.makeText(mContext, "Action is DragEvent.ACTION_DRAG_EXITED", Toast.LENGTH_SHORT).show();
+////                        notifyItemRangeInserted(getItemCount() - 1 - getItemCount(), getItemCount() - 1);
+//                        Collections.swap(items, fromPosition, toPosition);
+//
+////                        v.setLayoutParams(layoutParams);
+//                        break;
+//
+//                    case DragEvent.ACTION_DRAG_LOCATION  :
+////                        Toast.makeText(mContext, "Action is DragEvent.ACTION_DRAG_LOCATION", Toast.LENGTH_SHORT).show();
+////
+////                        x_cord = (int) event.getX();
+////                        y_cord = (int) event.getY();
+//                        break;
+//
+//                    case DragEvent.ACTION_DRAG_ENDED   :
+////                        Toast.makeText(mContext, "Action is DragEvent.ACTION_DRAG_ENDED", Toast.LENGTH_SHORT).show();
+//
+//
+//                        // Do nothing
+//                        break;
+//
+//                    case DragEvent.ACTION_DROP:
+//                        Toast.makeText(mContext, "ACTION_DROP event", Toast.LENGTH_SHORT).show();
+//                        Collections.swap(items, fromPosition, toPosition);
+//
+//
+//                        // Do nothing
+//                        break;
+//                    default: break;
+//                }
+//                return true;
+//            }
+//        });
     }
 
     @Override
@@ -74,47 +166,93 @@ public class ItemRecViewAdapter extends RecyclerView.Adapter<ItemRecViewAdapter.
         return items.size();
     }
 
-    public void setItems(ArrayList<Item> items) {
-        this.items = items;
+    public void setItems(ArrayList<Item> items,ArrayList<Integer> positions) {
+        ArrayList<Item> mostrando = new ArrayList<>();
+        positions.add(0,items.size()-1);
+        for (int i = 0;i<items.size();i++){
+            mostrando.add(items.get(positions.get(i)));
+        }
+        this.items = mostrando;
+        this.positions = positions;
+
         notifyDataSetChanged();
     }
 
-    public ArrayList<Item> getItems(int fromPosition, int toPosition ) {
-        Collections.swap(items,fromPosition,toPosition);
-        return this.items;
-    }
 
-    public void refresh(int fromPosition, int toPosition) {
-        Collections.swap(items,fromPosition,toPosition);
 
-        if(fromPosition<toPosition) {
-            for (int i = fromPosition; i < toPosition + 1; i++) {
+//    public void insertItem(ArrayList<Item> items) {
+//         positions.add(0,0);
+//         for (int i = 1;i<positions.size();i++){
+//             positions.set(i,positions.get(i)+1);
+//             items.get(i).setId(items.get(i).getId()+1);
+//        }
+//         correctItemsPositions(items);
+//    }
+public void adicionar(ArrayList<Item> items, ArrayList<Integer> positions ) {
+    positions.add(0,items.size()-1);
+    this.positions = positions;
+    this.items=items;
+//    correctItemsPositions(items);
+//SE A CONDIÇÃO PARA ORGANIZAR EM ORDEM ALFABÉTICA FOR SUPRIDA, FAZER O SEGUINTE
+//    for (int i =0;i<items.size();i++){
+//        int j=i;
+//        while(j>0 && items.get(j).getName().toLowerCase(Locale.ROOT).compareTo(items.get(j-1).getName().toLowerCase(Locale.ROOT))<0 ){
+//            Collections.swap(items, j,j-1);
+//            j--;
+//        }
+//    }
+    System.out.println("Novo item adicionado com sucesso!!!\n");
 
-                notifyItemChanged(i);
-            }
-        }else if(fromPosition>toPosition) {
-            for (int i = toPosition; i < fromPosition + 1; i++) {
-                notifyItemChanged(i);
-            }
-        }
+}
+
+//    public ArrayList<Item> getItems(int fromPosition, int toPosition ) {
+//    public ArrayList<Item> getItems() {
+//        Collections.swap(items,fromPosition,toPosition);
+//        notifyItemChanged(fromPosition);
+//        notifyItemChanged(toPosition);
+//
+//
+//        return this.items;
+//    }
+
+    public void refresh() {
+//        Collections.swap(items,fromPosition,toPosition);
+        notifyDataSetChanged();
+//        if(fromPosition<toPosition) {
+//            for (int i = fromPosition; i < toPosition + 1; i++) {
+//
+//                notifyItemChanged(i);
+//            }
+//        }else if(fromPosition>toPosition) {
+//            for (int i = toPosition; i < fromPosition + 1; i++) {
+//                notifyItemChanged(i);
+//            }
+//        }
     }
 
     public void setPositions(int fromPosition, int toPosition) {
-        Item temp;
-        if(fromPosition<toPosition) {
-            temp = items.get(fromPosition);
-            for (int i = fromPosition; i < toPosition+1; i++) {
-                items.set(i,items.get(i+1));
-            }
-            items.set(toPosition,temp);
-        }else if(fromPosition>toPosition) {
-            temp = items.get(toPosition);
-            for (int i = toPosition; i < fromPosition+1; i++) {
-                items.set(i,items.get(i+1));
-            }
-            items.set(fromPosition,temp);
-        }
+        this.fromPosition = fromPosition;
+        this.toPosition = toPosition;
+        Collections.swap(positions,fromPosition,toPosition);
+
+
+
     }
+
+    public void filterAtoZ() {
+        //todo: falta configurar o novo vetor de posições para essa configuração
+        //todo: falta o z to A
+        for (int i =0;i<items.size();i++){
+            int j=i;
+            while(j>0 && items.get(j).getName().toLowerCase(Locale.ROOT).compareTo(items.get(j-1).getName().toLowerCase(Locale.ROOT))<0 ){
+                Collections.swap(items, j,j-1);
+                Collections.swap(positions,j,j-1);
+                j--;
+            }
+        }
+        notifyDataSetChanged();
+    }
+
 
     public class ViewHolder extends RecyclerView.ViewHolder
     {
--- app/src/main/java/com/jogos/listadecompras/ItemsListActivity.java
+++ app/src/main/java/com/jogos/listadecompras/ItemsListActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,11 +24,13 @@ public class ItemsListActivity extends AppCompatActivity {
 
     private RecyclerView itemsRecView;
     private ItemRecViewAdapter adapter;
-    private ImageView imageNewItem;
-    private EditText edTxtPants, edTxtName;
+    private ImageView imgShowMore, imgShowMore2;
+    private EditText edTxtPants, edTxtName, edTxtBrand;
     private CheckBox checkBox;
+    private TextView txtShowMore;
 
     private ArrayList<Item> items;
+    private ArrayList<Integer> positions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,27 +40,39 @@ public class ItemsListActivity extends AppCompatActivity {
         adapter = new ItemRecViewAdapter(this);
         itemsRecView = findViewById(R.id.itensRecView);
         edTxtName = findViewById(R.id.edTxtName);
+        edTxtBrand = findViewById(R.id.edTxtBrand);
         edTxtPants = findViewById(R.id.edTxtPants);
         checkBox = findViewById(R.id.checkbox);
+        txtShowMore = findViewById(R.id.txtShowMore);
+        imgShowMore = findViewById(R.id.imgShowMore);
+        imgShowMore2 = findViewById(R.id.imgShowMore2);
+
         itemsRecView.setAdapter(adapter);
-        itemsRecView.setLayoutManager(new GridLayoutManager(this, 1));
+        itemsRecView.setLayoutManager(new LinearLayoutManager(this));
+
+
 
         items = new ArrayList<>();
+        positions = new ArrayList<>();
         items.add(new Item(0, "Banana", 6,"Natural", 6, "https://www.infoescola.com/wp-content/uploads/2010/04/banana_600797891.jpg",null, false));
+        positions.add(1);
         items.add(new Item(1, "Apple", 4,"Natural", 4, "https://www.collinsdictionary.com/images/thumb/apple_158989157_250.jpg",null, false));
+        positions.add(0);
         items.add(new Item(2, "Orange", 2,"Natural", 6, "https://riviste.newbusinessmedia.it/wp-content/uploads/sites/27/2013/12/Fotolia_11313277_M-300x264.jpg",null, false));
+        positions.add(2);
         items.add(new Item(3, "Papaya", 2,"Natural", 6, "https://comper.vteximg.com.br/arquivos/ids/182176-1000-1000/631906_mamao.jpg",null, false));
+        positions.add(3);
         items.add(new Item(4, "Melon", 2,"Natural", 6, "https://deluxeproduce.com/wp-content/uploads/2017/06/canary-melon.jpg",null,true));
+        positions.add(4);
         items.add(new Item(5, "Strawberry", 2,"Natural", 6, "https://www.collinsdictionary.com/images/full/strawberry_227472010.jpg",null, false));
+        positions.add(5);
         items.add(new Item(6, "Lemon", 2,"Natural", 6, "https://t4.ftcdn.net/jpg/02/55/39/77/360_F_255397744_rwNCund3WjKsrsv6yKKpK8tzmJ8sYRnF.jpg",null, false));
-
-        adapter.setItems(items);
+        positions.add(6);
 
 //        Parte do Drag and Drop
         ItemTouchHelper itemTouchHelper= new ItemTouchHelper(simpleCallback);
         itemTouchHelper.attachToRecyclerView(itemsRecView);
-
-
+        adapter.setItems(items, positions);
     }
 
     ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN,0 ) {
@@ -64,41 +82,11 @@ public class ItemsListActivity extends AppCompatActivity {
             int fromPosition = viewHolder.getAdapterPosition();
             int toPosition = target.getAdapterPosition();
 
-//            FUNCIONA MAS SÓ DEIXA ROLAR 5 LINHAS
-//                adapter.notifyItemMoved(fromPosition,toPosition);
-//                adapter.notifyItemChanged(fromPosition);
-//                adapter.notifyItemChanged(toPosition);
 
-//          muda o que está sendo exibido
             adapter.notifyItemMoved(fromPosition,toPosition);
-            Collections.swap(items, fromPosition, toPosition);
-            
+            adapter.setPositions(fromPosition,toPosition);
 
 
-//                adapter.setPositions(fromPosition, toPosition);
-
-                //muda o banco de dados
-//            Collections.swap(items, fromPosition, toPosition);
-
-                //atualiza o que está sendo exibido para o novo banco de dados.
-                //isso pooderia ser efetivado somente no ONDROP, porém ainda não controlo os eventos individualmente.
-
-
-//               adapter.setItems(items);
-
-
-//            adapter.setItems(Collections.swap(items, fromPosition, toPosition));
-                //               Collections.swap(items, fromPosition, toPosition);
-                //               recyclerView.getAdapter().notifyItemMoved(fromPosition,toPosition);
-
-      //      Item temp = items.get(fromPosition);
-
-
-//            items.remove(fromPosition);
-//            items.add(toPosition, temp);
-
-//            PADRÃO
-//            adapter.notifyItemMoved(fromPosition, toPosition);
 
             return true;
         }
@@ -112,27 +100,57 @@ public class ItemsListActivity extends AppCompatActivity {
 //                adapter.notifyItemRangeChanged(toPos,fromPos);
 //            }
 
+//            adapter.setPositions(fromPos,toPos);
+//            items= adapter.getItems();
+
 //            EU CRIEI UM MÉTODO QUE FAZ A MESMA COISA QUE O ITEMRANGECHANGED
 //            adapter.refresh(fromPos,toPos);
+//            adapter.notifyDataSetChanged();
 
+//            adapter.refresh(fromPos,toPos);
+//            Collections.swap(items, fromPos, toPos);
+//            adapter.notifyItemChanged(fromPos);
+//            adapter.notifyItemChanged(toPos);
         }
 
         @Override
         public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
 
         }
+
+
     };
 
-    public void onImgClicked(View view) {
+    public void onAddClicked(View view) {
+
+        int lastPosition = items.size();
+        items.add(lastPosition, new Item(lastPosition, edTxtName.getText().toString(), 84,"Natural", 100, "https://www.collinsdictionary.com/images/thumb/apple_158989157_250.jpg",null, false));
+
+        adapter.setItems(items, positions);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void moreOptions(View view) {
         if (edTxtPants.getVisibility()==View.VISIBLE){
             edTxtPants.setVisibility(View.GONE);
+            edTxtBrand.setVisibility(View.GONE);
+
+            imgShowMore.setRotation(0);
+            imgShowMore2.setRotation(0);
+
+
+            txtShowMore.setText("Show More");
         }else{
             edTxtPants.setVisibility(View.VISIBLE);
-        };
-        int lastPosition = items.size()-1;
-        items.add(0, new Item(lastPosition+1, edTxtName.getText().toString(), 84,"Natural", lastPosition, "https://www.collinsdictionary.com/images/thumb/apple_158989157_250.jpg",null, false));
-//        adapter.notifyItemInserted(0);
-        adapter.notifyDataSetChanged();
+            edTxtBrand.setVisibility(View.VISIBLE);
+            txtShowMore.setText("Show Less");
+            imgShowMore.setRotation(180);
+            imgShowMore2.setRotation(180);
 
+        };
+    }
+
+    public void reordenate(View view) {
+        adapter.filterAtoZ();
     }
 }
